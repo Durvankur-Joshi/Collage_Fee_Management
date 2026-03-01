21371b65cd122cf2536a60c17e0d4595a7f1a0b7
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2026-03-01 22:04:34 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
+app.options('*/', cors());
 
 app.use(express.json());
 app.use(morgan("dev"));
